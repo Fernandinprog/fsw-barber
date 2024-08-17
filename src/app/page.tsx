a9e7b093efdb7689d2566e943afe6cd5ff6360fb
Fernandinprog <nandogoiaba55@gmail.com>
2024-08-17 00:12:22 -0300

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -78,7 +78,7 @@ const Home = async () => {
       <div className="relative rounded-xl mt-6 h-[150px] w-full">
           <Image
             alt="Agende nos melhores com FSW Barber"
-            src="/public/banner.png"
+            src="/banner.png"
             fill
             className="rounded-xl object-cover"
           />
